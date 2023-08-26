35cb23539f42e30cc3fd611de58efc22733d19d7
Aman Upadhyay <amanxupadhyay@outlook.com>
2023-08-27 01:28:09 +0530

    Solved Calculator Task

--- Week-1/Javascript/hard/calculator.js
+++ Week-1/Javascript/hard/calculator.js
@@ -1,6 +1,6 @@
 /*
-  Implement a class `Calculator` having below methods
-    - initialise a result variable in the constructor and keep updating it after every arithmetic operation
+  TODO: Implement a class `Calculator` having below methods
+    - initialize a result variable in the constructor and keep updating it after every arithmetic operation
     - add: takes a number and adds it to the result
     - subtract: takes a number and subtracts it from the result
     - multiply: takes a number and multiply it to the result
@@ -9,14 +9,39 @@
     - getResult: returns the value of `result` variable
     - calculate: takes a string expression which can take multi-arithmetic operations and give its result
       example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
-      Points to Note: 
-        1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
-        2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
+    Points to Note:
+      1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
+      2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
 
   Once you've implemented the logic, test your code by running
   - `npm run test-calculator`
 */
 
-class Calculator {}
+class Calculator {
+  constructor() { this.result = 0; }
+
+  add(num) { this.result += num; }
+
+  subtract(num) { this.result -= num; }
+
+  multiply(num) { this.result *= num; }
+
+  divide(num) {
+    if (num === 0) throw new Error('Cannot divide by zero');
+    this.result /= num;
+  }
+
+  clear() { this.result = 0; }
+
+  getResult() { return this.result; }
+
+  calculate(expression) {
+    if(expression.includes("/ 0")) throw new Error('Cannot divide by zero');
+
+    this.result = eval(expression);
+
+    return this.result;
+  }
+}
 
 module.exports = Calculator;
